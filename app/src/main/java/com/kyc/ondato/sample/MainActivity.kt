f7591bfbf1ed4e26fe21d71b6275dd3e20c68fff
Arnoldas <armonas.arnoldas@gmail.com>
2021-05-01 15:04:41 +0300

    Initial commit

---
+++ app/src/main/java/com/kyc/ondato/sample/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.kyc.ondato.sample
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.kyc.ondato.Ondato
+import com.kyc.ondato.OndatoConfig
+import com.kyc.ondato.OndatoError
+import com.kyc.ondato.enums.Language
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initSdk()
+        findViewById<View>(R.id.start_button)?.setOnClickListener { startIdentification() }
+    }
+
+    private fun initSdk() {
+        val config = OndatoConfig.Builder()
+            .setToken("ADD YOUR TOKEN HERE")
+            .setIdentificationId("ADD IDENTIFICATION ID HERE")
+            .showSplashScreen(true)
+            .showStartScreen(true)
+            .showConsentScreen(true)
+            .showSelfieWithDocumentScreen(true)
+            .showSuccessScreen(true)
+            .ignoreLivenessError(false)
+            .ignoreVerificationErrors(false)
+            .recordProcess(true)
+            .setMode(OndatoConfig.Mode.TEST)
+            .setLanguage(Language.English)
+            .build()
+
+        Ondato.init(config)
+    }
+
+    private fun startIdentification() {
+        Ondato.starIdentification(this, object : Ondato.ResultListener {
+            override fun onSuccess(identificationId: String?) {
+                findViewById<TextView>(R.id.result_text_view)?.text = "Success"
+            }
+
+            override fun onFailure(identificationId: String?, error: OndatoError) {
+                findViewById<TextView>(R.id.result_text_view)?.text = "Failed"
+            }
+        })
+    }
+}
